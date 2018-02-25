726e8571db1f2420cda5ed74d1d8aa23fb55a232
wangpeng <594458910@qq.com>
2018-02-25 15:53:35 +0800

    第二章：身份验证

---
+++ src/Test/java/com/shiro/test/MyRealm3.java
@@ -0,0 +1,32 @@
+package com.shiro.test;
+
+import org.apache.shiro.authc.*;
+import org.apache.shiro.realm.Realm;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Creator : peng
+ * Date : 2018-02-25
+ * Time : 14:22
+ */
+public class MyRealm3 implements Realm {
+    public String getName() {
+        return "myrealm3";
+    }
+    public boolean supports(AuthenticationToken token) {
+        //仅支持UsernamePasswordToken类型的Token
+        return token instanceof UsernamePasswordToken;
+    }
+    public AuthenticationInfo getAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        String username = (String)token.getPrincipal();  //得到用户名
+        String password = new String((char[])token.getCredentials()); //得到密码
+        if(!"zhang".equals(username)) {
+            throw new UnknownAccountException(); //如果用户名错误
+        }
+        if(!"123".equals(password)) {
+            throw new IncorrectCredentialsException(); //如果密码错误
+        }
+        //如果身份认证验证成功，返回一个AuthenticationInfo实现；
+        return new SimpleAuthenticationInfo("zhang@163.com", "123", getName());
+    }
+}
